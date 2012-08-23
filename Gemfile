238d8562a59e55fda41b93256895ccf67774f2c8
Dustin Farris <dfarris@findwonder.com>
2012-08-23 12:11:48 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+group :development do
+  gem 'sqlite3', '1.3.6'
+  gem 'rspec-rails', '2.10.0'
+end
+    
+group :assets do
+  gem 'sass-rails',   '~> 3.2.5'
+  gem 'coffee-rails', '~> 3.2.2'
+  gem 'uglifier', '>= 1.2.7'
+end
+
+gem 'jquery-rails', '2.1.1'
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+group :production do
+  gem 'pg', '0.14.0'
+end
+
+# vim: sts=2 ts=2 ft=ruby
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jdknot::Application.config.secret_token = 'f7375c2306e9e75f94ce5df97a29ff4aa85868e4e88ec3320a1294dd9cd5647abb811186d58db9fde0d65dd7920d8cdeab8525b8fa2e8b01c39c2f0b1f10774a'
